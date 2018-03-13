02a816540b7564ffe838f4644def78076c02101f
Paul Farestveit <pfarestveit@berkeley.edu>
2018-03-13 15:30:46 -0700

    E-Grades Export tests part 2

--- pages/junction/canvas_e_grades_export_page.rb
+++ pages/junction/canvas_e_grades_export_page.rb
@@ -64,11 +64,40 @@ module Page
         wait_for_load_and_click_js continue_button_element
       end
 
+      # Sets the default grading scheme
+      # @param driver [Selenium::WebDriver]
+      # @param course [Course]
+      def set_default_grading_scheme(driver, course)
+        load_embedded_tool(driver, course)
+        click_set_default_scheme
+        click_continue
+        download_final_grades_element.when_visible Utils.medium_wait
+      end
+
+      # Un-mutes any muted assignments and sets the default grading scheme
+      # @param driver [Selenium::WebDriver]
+      # @param course [Course]
+      def resolve_all_issues(driver, course)
+        load_embedded_tool(driver, course)
+        required_adjustments_heading_element.when_present Utils.medium_wait
+        if required_adjustments_heading_element.visible?
+          logger.debug "Adjustments needed on site #{course.site_id}"
+          un_mute_all_cbx_element.when_present Utils.short_wait
+          sleep 1
+          click_un_mute_all if un_mute_all_cbx_element.visible?
+          click_set_default_scheme if set_scheme_cbx_element.visible?
+          click_continue
+          logger.debug 'Waiting for sections selection'
+          sections_select_element.when_visible Utils.medium_wait
+        else
+          logger.debug "No adjustments needed on site #{course.site_id}"
+        end
+      end
+
       # Selects a section for which to download the E-Grades CSV and preps the download dir to receive the file
       # @param section [Section]
       def choose_section(section)
         section_name = "#{section.course} #{section.label}"
-        logger.info "Downloading grades for #{section_name}"
         Utils.prepare_download_dir
         wait_for_element_and_select_js(sections_select_element, section_name)
       end
@@ -95,8 +124,9 @@ module Page
       # @param section [Section]
       # @return [Array<Hash>]
       def download_current_grades(course, section)
-        choose_section section
-        wait_for_update_and_click download_current_grades_element
+        logger.info "Downloading current grades for #{course.code} #{section.label}"
+        choose_section section if course.sections.length > 1
+        wait_for_load_and_click download_current_grades_element
         file_path = "#{Utils.download_dir}/egrades-current-#{section.id}-#{course.term.gsub(' ', '-')}-*.csv"
         csv = parse_grades_csv file_path
         csv.map { |r| r.to_hash }
@@ -107,8 +137,9 @@ module Page
       # @param section [Section]
       # @return [Array<Hash>]
       def download_final_grades(course, section)
-        choose_section section
-        wait_for_update_and_click download_final_grades_element
+        logger.info "Downloading final grades for #{course.code} #{section.label}"
+        choose_section section if course.sections.length > 1
+        wait_for_load_and_click download_final_grades_element
         file_path = "#{Utils.download_dir}/egrades-final-#{section.id}-#{course.term.gsub(' ', '-')}-*.csv"
         csv = parse_grades_csv file_path
         csv.map { |r| r.to_hash }
