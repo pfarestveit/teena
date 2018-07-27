47e0c54e76ad7867b92df65988712a724f06703c
Paul Farestveit <pfarestveit@berkeley.edu>
2018-07-27 13:40:55 -0700

    Scripts to collect BOAC SIS data and search performance data

---
+++ scripts/boac_cohort_filter_performance_script.rb
@@ -0,0 +1,87 @@
+require_relative '../util/spec_helper'
+
+begin
+
+  include Logging
+
+  performance_data = File.join(Utils.initialize_test_output_dir, "boac-search-performance-#{Utils.get_test_id}.csv")
+
+  dept = BOACUtils.test_dept
+  user = case dept
+              when BOACDepartments::ASC
+                get_dept_advisors(dept).first
+              when BOACDepartments::COE
+                get_dept_advisors(dept).first
+              else
+                User.new({:uid => Utils.super_admin_uid})
+            end
+
+  test_search_criteria = BOACUtils.get_test_search_criteria
+  unless dept == BOACDepartments::ASC
+    test_search_criteria.each { |c| c.squads = nil }
+    test_search_criteria.keep_if { |c| [c.levels, c.majors, c.gpa_ranges, c.units].compact.any? }
+  end
+
+  cohorts = test_search_criteria.map { |criteria| FilteredCohort.new({:search_criteria => CohortSearchCriteria.new(criteria)}) }
+
+  @driver = Utils.launch_browser
+  @analytics_page = ApiUserAnalyticsPage.new @driver
+  @homepage = Page::BOACPages::HomePage.new @driver
+  @cohort_page = Page::BOACPages::CohortPages::FilteredCohortListViewPage.new @driver
+  @homepage.dev_auth user
+
+  cohorts.each do |cohort|
+    begin
+
+      # SEARCHING
+
+      @homepage.click_sidebar_create_filtered
+      @cohort_page.perform_search(cohort, performance_data)
+
+      # SORTING
+
+      if cohort.member_count.zero?
+        logger.debug 'Skipping sort-by since there are no results'
+      else
+
+        # Team
+        if dept == BOACDepartments::ASC
+          @cohort_page.sort_by_team
+          @cohort_page.wait_for_spinner
+          @cohort_page.wait_for_student_list
+        end
+
+        # First Name
+        @cohort_page.sort_by_first_name
+        @cohort_page.wait_for_spinner
+        @cohort_page.wait_for_student_list
+
+        # GPA
+        @cohort_page.sort_by_gpa
+        @cohort_page.wait_for_spinner
+        @cohort_page.wait_for_student_list
+
+        # Level
+        @cohort_page.sort_by_level
+        @cohort_page.wait_for_spinner
+        @cohort_page.wait_for_student_list
+
+        # Major
+        @cohort_page.sort_by_major
+        @cohort_page.wait_for_spinner
+        @cohort_page.wait_for_student_list
+
+        # Units
+        @cohort_page.sort_by_units
+        @cohort_page.wait_for_spinner
+        @cohort_page.wait_for_student_list
+
+      end
+    rescue => e
+      Utils.log_error e
+    end
+  end
+
+ensure
+  Utils.quit_browser @driver
+end
---
+++ scripts/boac_user_data_script.rb
@@ -0,0 +1,105 @@
+require_relative '../util/spec_helper'
+
+begin
+
+  include Logging
+
+  user_profile_data_heading = %w(UID Name PreferredName Email Phone Units GPA Level Colleges Majors Terms Writing History Institutions Cultures Graduation Alerts)
+  user_profile_sis_data = Utils.create_test_output_csv('boac-sis-profiles.csv', user_profile_data_heading)
+
+  user_course_data_heading = %w(UID Term CourseCode CourseName SectionCcn SectionCode Primary? Midpoint Grade GradingBasis Units EnrollmentStatus)
+  user_course_sis_data = Utils.create_test_output_csv('boac-sis-courses.csv', user_course_data_heading)
+
+  @driver = Utils.launch_browser
+  @boac_homepage = Page::BOACPages::HomePage.new @driver
+  @boac_homepage.dev_auth
+
+  students = NessieUtils.get_all_relevant_students
+  logger.info "There are #{students.length} students"
+  students.each do |student|
+    begin
+
+      logger.info "Checking UID #{student.uid}"
+      user_analytics_data = ApiUserAnalyticsPage.new @driver
+      user_analytics_data.get_data(@driver, student)
+      analytics_api_sis_data = user_analytics_data.user_sis_data
+
+      alerts = BOACUtils.get_students_alerts [student]
+      alert_msgs = alerts.map &:message
+
+      terms = user_analytics_data.terms
+      if terms.any?
+        begin
+          terms.each do |term|
+            begin
+
+              term_name = user_analytics_data.term_name term
+              courses = user_analytics_data.courses term
+              term_section_ccns = []
+
+              if courses.any?
+                courses.each do |course|
+                  begin
+
+                    course_sis_data = user_analytics_data.course_sis_data course
+                    course_code = course_sis_data[:code]
+                    sections = user_analytics_data.sections course
+                    sections.each do |section|
+                      begin
+
+                        section_sis_data = user_analytics_data.section_sis_data section
+                        term_section_ccns << section_sis_data[:ccn]
+
+                        row = [student.uid, term_name, course_code, course_sis_data[:title], section_sis_data[:ccn], "#{section_sis_data[:component]} #{section_sis_data[:number]}",
+                               section_sis_data[:primary], course_sis_data[:midpoint], course_sis_data[:grade], course_sis_data[:grading_basis], course_sis_data[:units], section_sis_data[:status]]
+                        Utils.add_csv_row(user_course_sis_data, row)
+                      rescue => e
+                        BOACUtils.log_error_and_screenshot(@driver, e, "#{student.uid}-#{term_name}-#{course_code}-#{section_sis_data[:ccn]}")
+                      end
+                    end
+
+                    drops = user_analytics_data.dropped_sections term
+                    if drops
+                      drops.each do |drop|
+                        row = [student.uid, term_name, drop[:title], nil, nil, drop[:number], nil, nil, nil, nil, nil, 'D']
+                        Utils.add_csv_row(user_course_sis_data, row)
+                      end
+                    end
+
+                  rescue => e
+                    BOACUtils.log_error_and_screenshot(@driver, e, "#{student.uid}-#{term_name}")
+                  end
+                end
+
+              else
+                logger.info "UID #{student.uid} has no courses in term #{term_name}"
+              end
+            rescue => e
+              BOACUtils.log_error_and_screenshot(@driver, e, "#{student.uid}")
+            end
+          end
+        rescue => e
+          BOACUtils.log_error_and_screenshot(@driver, e, "#{student.uid}")
+        end
+      else
+        logger.info "UID #{student.uid} has no terms"
+      end
+    rescue => e
+      BOACUtils.log_error_and_screenshot(@driver, e, "#{student.uid}")
+    ensure
+      if analytics_api_sis_data
+        row = [student.uid, student.full_name, analytics_api_sis_data[:preferred_name], analytics_api_sis_data[:email],
+               analytics_api_sis_data[:phone], analytics_api_sis_data[:cumulative_units], analytics_api_sis_data[:cumulative_gpa], analytics_api_sis_data[:level],
+               analytics_api_sis_data[:colleges] && analytics_api_sis_data[:colleges] * '; ', analytics_api_sis_data[:majors] && analytics_api_sis_data[:majors] * '; ',
+               analytics_api_sis_data[:terms_in_attendance], analytics_api_sis_data[:reqt_writing], analytics_api_sis_data[:reqt_history],
+               analytics_api_sis_data[:reqt_institutions], analytics_api_sis_data[:reqt_cultures], analytics_api_sis_data[:expected_graduation], alert_msgs]
+        Utils.add_csv_row(user_profile_sis_data, row)
+      end
+    end
+  end
+
+rescue => e
+  Utils.log_error e
+ensure
+  Utils.quit_browser @driver
+end
