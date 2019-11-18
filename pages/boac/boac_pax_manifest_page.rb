d0ab07a19aee8adaa7604aca8f6f26699e6e3640
Anne Lyttle <alyttle@berkeley.edu>
2019-11-18 10:38:52 -0800

    BOAC-2935: Updates tests for passenger manifest redesign

--- pages/boac/boac_pax_manifest_page.rb
+++ pages/boac/boac_pax_manifest_page.rb
@@ -28,32 +28,34 @@ class BOACPaxManifestPage
 
   # Filters
 
-  text_area(:user_search_input, id: 'user-name-uid-search')
+  select_list(:filter_mode_select, id: 'user-filter-options')
+  text_area(:user_search_input, id: 'search-user-input')
+  elements(:autocomplete_names, :link, xpath: '//a[contains(@id, "search-user-suggestion-")]')
+  select_list(:permissions_select, id: 'user-permission-options')
+  select_list(:status_select, id: 'user-status-options')
   select_list(:dept_select, id: 'department-select-list')
-  elements(:filter_checkbox, :text_area, xpath: '//input[contains(@id, "user-permission-options")]')
-  checkbox(:admins_cbx, xpath: '//input[@value="isAdmin"]')
-  checkbox(:advisors_cbx, xpath: '//input[@value="isAdvisor"]')
-  checkbox(:canvas_access_cbx, xpath: '//input[@value="canAccessCanvasData"]')
-  checkbox(:directors_cbx, xpath: '//input[@value="isDirector"]')
-  checkbox(:drop_in_advisors_cbx, xpath: '//input[@value="isDropInAdvisor"]')
-  checkbox(:schedulers_cbx, xpath: '//input[@value="isScheduler"]')
-  checkbox(:active_cbx, xpath: '//input[@value="isActive"]')
-  checkbox(:deleted_cbx, xpath: '//input[@value="deletedAt"]')
-  checkbox(:blocked_cbx, xpath: '//input[@value="isBlocked"]')
-  checkbox(:expired_cbx, xpath: '//input[@value="isExpiredPerLdap"]')
-  button(:reset_filters_button, id: 'user-filter-reset-button')
+
+  # Sets text in a given element and waits for and clicks the first auto-suggest result
+  # @param element [PageObject::Element]
+  # @param name [String]
+  def set_first_auto_suggest(element, name)
+    wait_for_element_and_type(element, name)
+    sleep Utils.click_wait
+    link_element = auto_suggest_option_elements.first
+    wait_for_load_and_click link_element
+  end
 
   # Enters an advisor UID in the user search input
   # @param advisor [BOACUser]
   def search_for_advisor(advisor)
     logger.info "Searching for advisor UID #{advisor.uid}"
-    wait_for_element_and_type(user_search_input_element, advisor.uid)
+    wait_for_element(user_search_input_element, Utils.medium_wait)
+    set_first_auto_suggest(user_search_input_element, advisor.uid)
   end
 
-  # Clicks the Reset Filter button
-  def reset_filters
-    logger.info 'Resetting filters'
-    wait_for_update_and_click reset_filters_button_element unless reset_filters_button_element.disabled?
+  # Selects the Filter option
+  def select_filter_mode
+    wait_for_element_and_select_js(filter_mode_select_element, 'Filter')
   end
 
   # Selects the All Departments option
@@ -69,42 +71,17 @@ class BOACPaxManifestPage
     wait_for_element_and_select_js(dept_select_element, dept.name)
   end
 
-  # Clicks the checkbox for a filter with a given label
-  # @param label [String]
-  def toggle_checkbox_filter(label)
-    js_click checkbox_element(xpath: "//label[contains(.,'#{label}')]/preceding-sibling::input")
-  end
-
-  # Clicks the Reset Filter button and then clicks the unchecked filter checkboxes, effectively checking all boxes
-  def check_all_filters
-    logger.info 'Checking all filter checkboxes'
-    reset_filters
-    deleted_cbx_element.when_present Utils.short_wait
-    js_click deleted_cbx_element
-    js_click blocked_cbx_element
-    js_click expired_cbx_element
-  end
-
-  # Clicks the Reset Filter button and then clicks the checked filter checkboxes, effectively un-checking all the boxes
-  def uncheck_all_filters
-    logger.info 'Un-checking all filter checkboxes'
-    reset_filters
-    admins_cbx_element.when_present Utils.short_wait
-    js_click admins_cbx_element
-    js_click advisors_cbx_element
-    js_click canvas_access_cbx_element
-    js_click directors_cbx_element
-    js_click drop_in_advisors_cbx_element
-    js_click schedulers_cbx_element
-    js_click active_cbx_element
+  # Selects the BOA Admin option
+  def select_admin_mode
+    wait_for_element_and_select_js(filter_mode_select_element, 'BOA Admins')
   end
 
   # Advisor list
 
-  elements(:advisor_uid, :div, xpath: '//div[contains(@id, "uid-")]')
+  elements(:advisor_uid, :span, xpath: '//span[contains(@id, "uid-")]')
   elements(:advisor_name, :link, xpath: '//a[contains(@id, "directory-link-")]')
-  elements(:advisor_title, :div, xpath: '//div[contains(@id, "title-")]')
-  elements(:advisor_email, :div, xpath: '//div[contains(@id, "email-")]')
+  elements(:advisor_dept, :span, xpath: '//span[contains(@id, "dept-")]')
+  elements(:advisor_email, :link, xpath: '//a[contains(@aria-label, "Send email")]')
 
   # Waits briefly for at least one advisor row element to be present
   def wait_for_advisor_list
@@ -118,35 +95,14 @@ class BOACPaxManifestPage
   # @return [Array<String>]
   def list_view_uids
     wait_for_advisor_list
-    visible_uids = []
-    page_count = list_view_page_count
-    page = 1
-    if page_count == 1
-      logger.debug 'There is 1 page'
-      visible_uids << advisor_uid_elements.map(&:text)
-    else
-      logger.debug "There are #{page_count} pages"
-      visible_uids << advisor_uid_elements.map(&:text)
-      (page_count - 1).times do
-        page += 1
-        wait_for_update_and_click go_to_next_page_link_element
-        wait_until(Utils.medium_wait) { advisor_uid_elements.any? }
-        visible_uids << advisor_uid_elements.map(&:text)
-      end
-    end
-    visible_uids.flatten
-  end
-
-  # Sorts an advisor result set by UID (asc or desc, whichever applies to the current state of the list)
-  def sort_by_uid
-    sort_by_option 'Uid'
+    advisor_uid_elements.map(&:text)
   end
 
   # Returns all the department names shown for a given user row
   # @param user [BOACUser]
   # @return [Array<String>]
   def visible_advisor_depts(user)
-    dept_els = browser.find_elements(xpath: "//span[contains(@id, 'dept-') and contains(@id, '-#{user.uid}')]")
+    dept_els = browser.find_elements(xpath: "//span[contains(@id, 'dept-') and contains(@id, '-#{user.uid}')]/span")
     dept_els.map &:text
   end
 
@@ -156,60 +112,20 @@ class BOACPaxManifestPage
     wait_for_update_and_click button_element(id: "user-#{user.uid}-details-toggle")
   end
 
-  # Returns the Canvas permission for a given user row
+  # Returns the user details data for a given user row
   # @param user [BOACUser]
-  # @return [String]
-  def visible_canvas_perm(user)
-    canvas_el = list_item_element(id: "permission-canvas-data-#{user.uid}")
-    canvas_el.text if canvas_el.exists?
-  end
-
-  # Returns the Admin status for a given user row
-  # @param user [BOACUser]
-  # @return [String]
-  def visible_admin_perm(user)
-    admin_el = list_item_element(id: "permission-admin-#{user.uid}")
-    admin_el.text if admin_el.exists?
-  end
-
-  # Returns the deleted status for a given user row
-  # @param user [BOACUser]
-  # @return [String]
-  def visible_deleted_status(user)
-    status_el = list_item_element(id: "status-deleted-#{user.uid}")
-    status_el.text if status_el.exists?
-  end
-
-  # Returns the blocked status for a given user row
-  # @param user [BOACUser]
-  # @return [String]
-  def visible_blocked_status(user)
-    status_el = list_item_element(id: "status-blocked-#{user.uid}")
-    status_el.text if status_el.exists?
-  end
-
-  # Returns the expired status for a given user row
-  # @param user [BOACUser]
-  # @return [String]
-  def visible_expired_status(user)
-    status_el = list_item_element(id: "status-expired-#{user.uid}")
-    status_el.text if status_el.exists?
+  # @return [Hash]
+  def get_user_details(user)
+    details_el = browser.find_element(id: "user-details-#{user.uid}")
+    JSON.parse details_el.text
   end
 
   # Returns the department roles for a given user row
   # @param user [BOACUser]
   # @return [Array<String>]
-  def visible_dept_roles(user)
-    dept_role_els = browser.find_elements(xpath: "//td[contains(@id, 'dept-roles-') and contains(@id, '-#{user.uid}')]")
-    dept_role_els.map &:text
-  end
-
-  # Returns the department membership types for a given user row
-  # @param user [BOACUser]
-  # @return [Array<String>]
-  def visible_dept_memberships(user)
-    dept_memb_els = browser.find_elements(xpath: "//td[contains(@id, 'dept-membership-') and contains(@id, '-#{user.uid}')]")
-    dept_memb_els.map &:text
+  def visible_dept_roles(user, dept_code)
+    dept_role_el = browser.find_element(id: "dept-#{dept_code}-#{user.uid}")
+    dept_role_el.text
   end
 
   # Returns link to dept tab on admin page
--- spec/boac/boac_user_mgmt_spec.rb
+++ spec/boac/boac_user_mgmt_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../../util/spec_helper'
 
-describe 'The BOAC users tool' do
+describe 'The BOAC passenger manifest' do
 
   include Logging
 
@@ -17,253 +17,145 @@ describe 'The BOAC users tool' do
 
     @homepage.dev_auth
     @homepage.click_pax_manifest_link
-    @admin_page.user_search_input_element.when_visible Utils.short_wait
+    @admin_page.filter_mode_select_element.when_visible Utils.medium_wait
   end
 
   after(:all) { Utils.quit_browser @driver }
 
-  it 'defaults to all active users with any permissions' do
-    expect(@admin_page.dept_select).to eql('-- Select a department --')
-    expect(@admin_page.admins_cbx_checked?).to be true
-    expect(@admin_page.advisors_cbx_checked?).to be true
-    expect(@admin_page.canvas_access_cbx_checked?).to be true
-    expect(@admin_page.directors_cbx_checked?).to be true
-    expect(@admin_page.drop_in_advisors_cbx_checked?).to be true
-    expect(@admin_page.schedulers_cbx_checked?).to be true
-    expect(@admin_page.active_cbx_checked?).to be true
-    expect(@admin_page.deleted_cbx_checked?).to be false
-    expect(@admin_page.blocked_cbx_checked?).to be false
-    expect(@admin_page.expired_cbx_checked?).to be false
+  it 'defaults to user search mode' do
+    expect(@admin_page.filter_mode_select).to eql('Search')
+    expect(@admin_page.user_search_input_element.visible?).to be true
   end
 
-  it 'shows all departments' do
-    expected_options = ['-- Select a department --', 'All Departments'] + non_admin_depts.map(&:name).sort
-    logger.debug "Expected: #{expected_options}"
-    logger.debug "Actual: #{@admin_page.dept_select_options}"
-    expect(@admin_page.dept_select_options).to eql(expected_options)
+  context 'in user search mode' do
+    # Inactive users don't come up in search results, so don't try to look for them
+    # TODO - search for inactive users if BOAC-2882 is fixed
+    auth_users.select { |u| u.uid.length == 7 && u.active }.shuffle.last(25).each do |user|
+      context "searching for UID #{user.uid}" do
+        before(:all) do
+          @admin_page.search_for_advisor user
+          @admin_page.wait_for_advisor_list
+          @admin_page.expand_user_row user
+        end
+
+        it("shows a search result for UID #{user.uid}") { expect(@admin_page.list_view_uids.first).to eql(user.uid) }
+
+        it("shows the department(s) for UID #{user.uid}") { expect(@admin_page.visible_advisor_depts(user).sort).to eql(user.depts.map(&:name).uniq.sort) }
+
+        it "shows the department role(s) for UID #{user.uid}" do
+          user.advisor_roles.each do |dept_role|
+            expected_roles = []
+            expected_roles << 'Advisor' if dept_role.is_advisor
+            expected_roles << 'Director' if dept_role.is_director
+            expected_roles << 'Scheduler' if dept_role.is_scheduler
+            expected_roles << 'Drop-In Advisor' if dept_role.is_drop_in_advisor
+            visible_dept_roles = @admin_page.visible_dept_roles(user, dept_role.dept.code)
+            visible_roles = visible_dept_roles[/.*\(([^\)]*)/,1]
+            actual_roles = visible_roles.split(', ')
+            actual_roles.concat(actual_roles.pop.split(' and '))
+            expect(actual_roles).to eql(expected_roles)
+          end
+        end
+
+        it "shows the right Canvas permission for UID #{user.uid}" do
+          visible_user_details = @admin_page.get_user_details user
+          expect(visible_user_details['canAccessCanvasData']).to eql(user.can_access_canvas_data)
+        end
+
+        it "shows the right admin permission for UID #{user.uid}" do
+          visible_user_details = @admin_page.get_user_details user
+          expect(visible_user_details['isAdmin']).to eql(user.is_admin)
+        end
+
+        it "shows the active/deleted status for UID #{user.uid}" do
+          visible_user_details = @admin_page.get_user_details user
+          expect(visible_user_details['deletedAt'] ? false : true).to eql(user.active)
+        end
+
+        it "shows the right blocked status for UID #{user.uid}" do
+          visible_user_details = @admin_page.get_user_details user
+          expect(visible_user_details['isBlocked']).to eql(user.is_blocked)
+        end
+
+        it "shows the department membership type(s) for UID #{user.uid}" do
+          visible_user_details = @admin_page.get_user_details user
+          user.advisor_roles.each do |dept_role|
+            visible_dept = visible_user_details['departments'].find { |d| d['code'] == dept_role.dept.code}
+            expect(visible_dept['automateMembership']).to eql(dept_role.is_automated)
+          end
+        end
+
+        it "shows a 'become' link if UID #{user.uid} is active" do
+          has_become_link = @admin_page.become_user_link_element(user).exists?
+          user.active ? (expect(has_become_link).to be true) : (expect(has_become_link).to be false)
+        end
+      end
+    end
   end
 
-  non_admin_depts.each do |dept|
-    it "shows all the advisors in #{dept.name}" do
-      logger.info "Checking advisor list for #{dept.name}"
-      dept_advisors = auth_users.select { |u| u.depts.include? dept }
-      @admin_page.check_all_filters
-      @admin_page.select_dept dept
-      expected_uids = dept_advisors.map(&:uid).sort
+  context 'in user filter mode' do
+    before { @admin_page.select_filter_mode }
+
+    it 'shows all departments' do
+      expected_options = ['All'] + non_admin_depts.map(&:name).sort
+      expect(@admin_page.dept_select_options).to eql(expected_options)
+    end
+
+    non_admin_depts.each do |dept|
+      it "shows all the advisors in #{dept.name}" do
+        logger.info "Checking advisor list for #{dept.name}"
+        dept_advisors = auth_users.select { |u| u.depts.include? dept }
+        @admin_page.select_dept dept
+        expected_uids = dept_advisors.map(&:uid).sort
+        @admin_page.wait_for_advisor_list
+        visible_uids = @admin_page.list_view_uids.sort
+        @admin_page.wait_until(1, "Expected but not present: #{expected_uids - visible_uids}, present but not expected: #{visible_uids - expected_uids}") do
+          visible_uids == expected_uids
+        end
+      end
+    end
+
+    it 'shows some advisor names' do
+      @admin_page.select_all_depts
+      @admin_page.wait_for_advisor_list
+      visible_names = @admin_page.advisor_name_elements.map &:text
+      visible_names.keep_if { |n| !n.empty? }
+      expect(visible_names).not_to be_empty
+    end
+
+    it 'shows some advisor department titles' do
+      visible_dept_titles = @admin_page.advisor_dept_elements.map &:text
+      visible_dept_titles.keep_if { |t| !t.empty? }
+      expect(visible_dept_titles).not_to be_empty
+    end
+
+    it 'shows some advisor email addresses' do
+      visible_emails = @admin_page.advisor_email_elements.map { |e| e.attribute('href') }
+      visible_emails.keep_if { |e| !e.empty? }
+      expect(visible_emails).not_to be_empty
+    end
+
+    # TODO it 'allows an admin to sort users by Last Name ascending'
+    # TODO it 'allows an admin to sort users by Last Name descending'
+    # TODO it 'allows an admin to sort users by Last Login ascending'
+    # TODO it 'allows an admin to sort users by Last Login descending'
+  end
+
+  context 'in BOA Admin mode' do
+    before { @admin_page.select_admin_mode }
+
+    it "shows all the admins" do
+      dept = BOACDepartments::ADMIN
+      logger.info "Checking users list for #{dept.name}"
+      admin_users = auth_users.select { |u| u.is_admin }
+      expected_uids = admin_users.map(&:uid).sort
+      @admin_page.wait_for_advisor_list
       visible_uids = @admin_page.list_view_uids.sort
       @admin_page.wait_until(1, "Expected but not present: #{expected_uids - visible_uids}, present but not expected: #{visible_uids - expected_uids}") do
         visible_uids == expected_uids
       end
     end
   end
-
-  it 'shows some advisor names' do
-    @admin_page.check_all_filters
-    @admin_page.select_all_depts
-    @admin_page.wait_for_advisor_list
-    @admin_page.sort_by_uid
-    visible_names = @admin_page.advisor_name_elements.map &:text
-    logger.debug "Visible names: #{visible_names}"
-    visible_names.keep_if { |n| !n.empty? }
-    expect(visible_names).not_to be_empty
-  end
-
-  it 'shows some advisor titles' do
-    visible_titles = @admin_page.advisor_title_elements.map &:text
-    logger.debug "Visible titles: #{visible_titles}"
-    visible_titles.keep_if { |t| !t.empty? }
-    expect(visible_titles).not_to be_empty
-  end
-
-  it 'shows some advisor email addresses' do
-    visible_emails = @admin_page.advisor_email_elements.map &:text
-    logger.debug "Visible emails: #{visible_emails}"
-    visible_emails.keep_if { |e| !e.empty? }
-    expect(visible_emails).not_to be_empty
-  end
-
-  # Inactive users don't come up in search results, so don't try to look for them
-  # TODO - search for inactive users if BOAC-2882 is fixed
-  auth_users.select { |u| u.uid.length == 7 && u.active }.shuffle.last(25).each do |user|
-
-    it "allows an admin to search for UID #{user.uid}" do
-      @admin_page.search_for_advisor user
-      @admin_page.wait_for_advisor_list
-      @admin_page.wait_until(1) { @admin_page.list_view_uids.length == 1 }
-      expect(@admin_page.list_view_uids.first).to eql(user.uid)
-    end
-
-    it("shows the department(s) for UID #{user.uid}") { expect(@admin_page.visible_advisor_depts(user).sort).to eql(user.depts.map(&:name).sort) }
-
-    it "shows the right Canvas permission for UID #{user.uid}" do
-      @admin_page.expand_user_row user
-      expected_permission = user.can_access_canvas_data ? 'Canvas data access' : 'No Canvas data'
-      expect(@admin_page.visible_canvas_perm user).to eql(expected_permission)
-    end
-
-    it "shows the right admin permission for UID #{user.uid}" do
-      expected_permission = 'Admin' if user.is_admin
-      expect(@admin_page.visible_admin_perm user).to eql(expected_permission)
-    end
-
-    it "shows the active/deleted status for UID #{user.uid}" do
-      expected_status = user.active ? 'Active' : 'Deleted'
-      expect(@admin_page.visible_deleted_status user).to eql(expected_status)
-    end
-
-    it "shows the right blocked status for UID #{user.uid}" do
-      expected_status = 'Blocked' if user.is_blocked
-      expect(@admin_page.visible_blocked_status user).to eql(expected_status)
-    end
-
-    it "shows the department role(s) for UID #{user.uid}" do
-      expected_roles = []
-      expected_roles << 'Advisor' if user.advisor_roles.find(&:is_advisor)
-      expected_roles << 'Director' if user.advisor_roles.find(&:is_director)
-      expected_roles << 'Scheduler' if user.advisor_roles.find(&:is_scheduler)
-      expected_roles << 'Drop-In Advisor' if user.advisor_roles.find(&:is_drop_in_advisor)
-      expect(@admin_page.visible_dept_roles(user).uniq.sort).to eql(expected_roles.sort)
-    end
-
-    it "shows the department membership type(s) for UID #{user.uid}" do
-      expected_types = []
-      if user.advisor_roles.map(&:is_automated).compact.empty?
-        expect(@admin_page.visible_dept_memberships user).to be_empty
-      else
-        expected_types << 'Automated Membership' if (user.advisor_roles.find { |r| r.is_automated })
-        expected_types << 'Manual Membership' if (user.advisor_roles.find { |r| !r.is_automated })
-        expect(@admin_page.visible_dept_memberships(user).uniq.sort).to eql(expected_types.sort)
-      end
-    end
-
-    it "shows a 'become' link if UID #{user.uid} is active" do
-      has_become_link = @admin_page.become_user_link_element(user).exists?
-      user.active ? (expect(has_become_link).to be true) : (expect(has_become_link).to be false)
-    end
-  end
-
-  context 'when filtering the advisor list' do
-
-    before(:each) { @admin_page.reset_filters }
-
-    it 'allows an admin to filter for Admins only' do
-      @admin_page.toggle_checkbox_filter 'Advisors'
-      @admin_page.toggle_checkbox_filter 'Canvas Access'
-      @admin_page.toggle_checkbox_filter 'Directors'
-      @admin_page.toggle_checkbox_filter 'Drop-In Advisors'
-      @admin_page.toggle_checkbox_filter 'Schedulers'
-      # TODO - the expectation
-    end
-
-    it 'allows an admin to filter out Admins' do
-      @admin_page.toggle_checkbox_filter 'Admins'
-      # TODO - the expectation
-    end
-
-    it 'allows an admin to filter for Advisors only' do
-      @admin_page.toggle_checkbox_filter 'Admins'
-      @admin_page.toggle_checkbox_filter 'Canvas Access'
-      @admin_page.toggle_checkbox_filter 'Directors'
-      @admin_page.toggle_checkbox_filter 'Drop-In Advisors'
-      @admin_page.toggle_checkbox_filter 'Schedulers'
-      # TODO - the expectation
-    end
-
-    it 'allows an admin to filter out Advisors' do
-      @admin_page.toggle_checkbox_filter 'Advisors'
-      # TODO - the expectation
-    end
-
-    it 'allows an admin to filter for Canvas Access only' do
-      @admin_page.toggle_checkbox_filter 'Admins'
-      @admin_page.toggle_checkbox_filter 'Advisors'
-      @admin_page.toggle_checkbox_filter 'Directors'
-      @admin_page.toggle_checkbox_filter 'Drop-In Advisors'
-      @admin_page.toggle_checkbox_filter 'Schedulers'
-      # TODO - the expectation
-    end
-
-    it 'allows an admin to filter out Canvas Access' do
-      @admin_page.toggle_checkbox_filter 'Canvas Access'
-      # TODO - the expectation
-    end
-
-    it 'allows an admin to filter for Directors only' do
-      @admin_page.toggle_checkbox_filter 'Admins'
-      @admin_page.toggle_checkbox_filter 'Advisors'
-      @admin_page.toggle_checkbox_filter 'Canvas Access'
-      @admin_page.toggle_checkbox_filter 'Drop-In Advisors'
-      @admin_page.toggle_checkbox_filter 'Schedulers'
-      # TODO - the expectation
-    end
-
-    it 'allows an admin to filter out Directors' do
-      @admin_page.toggle_checkbox_filter 'Directors'
-      # TODO - the expectation
-    end
-
-    it 'allows an admin to filter for Drop-In Advisors only' do
-      @admin_page.toggle_checkbox_filter 'Admins'
-      @admin_page.toggle_checkbox_filter 'Advisors'
-      @admin_page.toggle_checkbox_filter 'Canvas Access'
-      @admin_page.toggle_checkbox_filter 'Directors'
-      @admin_page.toggle_checkbox_filter 'Schedulers'
-      # TODO - the expectation
-    end
-
-    it 'allows an admin to filter out Drop-In Advisors' do
-      @admin_page.toggle_checkbox_filter 'Drop-In Advisors'
-      # TODO - the expectation
-    end
-
-    it 'allows an admin to filter for Schedulers only' do
-      @admin_page.toggle_checkbox_filter 'Admins'
-      @admin_page.toggle_checkbox_filter 'Advisors'
-      @admin_page.toggle_checkbox_filter 'Canvas Access'
-      @admin_page.toggle_checkbox_filter 'Directors'
-      @admin_page.toggle_checkbox_filter 'Drop-In Advisors'
-      # TODO - the expectation
-    end
-
-    it 'allows an admin to filter out Schedulers' do
-      @admin_page.toggle_checkbox_filter 'Schedulers'
-      # TODO - the expectation
-    end
-
-    it 'allows an admin to filter for Active users only' do
-      # TODO - the expectation
-    end
-
-    it 'allows an admin to filter out Active users' do
-      @admin_page.toggle_checkbox_filter 'Active'
-      @admin_page.toggle_checkbox_filter 'Deleted'
-      @admin_page.toggle_checkbox_filter 'Blocked'
-      @admin_page.toggle_checkbox_filter 'Expired'
-      # TODO - the expectation
-    end
-
-    it 'allows an admin to filter for Deleted users only' do
-      @admin_page.toggle_checkbox_filter 'Active'
-      @admin_page.toggle_checkbox_filter 'Deleted'
-      # TODO - the expectation
-    end
-
-    it 'allows an admin to filter for Blocked Active users only' do
-      @admin_page.toggle_checkbox_filter 'Blocked'
-      # TODO - the expectation
-    end
-
-    it 'allows an amdin to filter for Expired users only' do
-      # TODO - the expectation
-    end
-  end
-
-  # TODO it 'allows an admin to sort users by UID ascending'
-  # TODO it 'allows an admin to sort users by UID descending'
-  # TODO it 'allows an admin to sort users by Name ascending'
-  # TODO it 'allows an admin to sort users by Name descending'
-  # TODO it 'allows an admin to sort users by Title ascending'
-  # TODO it 'allows an admin to sort users by Title descending'
 
   context 'exporting all BOA users' do
 
@@ -273,12 +165,14 @@ describe 'The BOAC users tool' do
       it "exports all #{dept[:dept].name} users" do
         dept_user_uids = dept[:advisors].map &:uid
         csv_dept_user_uids = @csv.map do |r|
-          if r[:dept_code] == dept[:dept].code && r[:dept_name] == (dept[:dept].export_name || dept[:dept].name)
+          if r[:departments].include? dept[:dept].code
             r[:uid].to_s
           end
         end
-        logger.debug "Unexpected #{dept[:dept].name} advisors: #{csv_dept_user_uids.compact - dept_user_uids}"
-        logger.debug "Missing #{dept[:dept].name} advisors: #{dept_user_uids - csv_dept_user_uids.compact}"
+        unexpected_advisors = csv_dept_user_uids.compact - dept_user_uids
+        missing_advisors = dept_user_uids - csv_dept_user_uids.compact
+        logger.debug "Unexpected #{dept[:dept].name} advisors: #{unexpected_advisors}" unless unexpected_advisors.empty?
+        logger.debug "Missing #{dept[:dept].name} advisors: #{missing_advisors}" unless missing_advisors.empty?
         expect(csv_dept_user_uids.compact.sort).to eql(dept_user_uids.sort)
       end
     end
@@ -286,16 +180,37 @@ describe 'The BOAC users tool' do
     it 'generates valid data' do
       first_names = []
       last_names = []
+      uids = []
+      titles = []
       emails = []
+      departments = []
+      drop_in_advising_flags = []
+      can_access_canvas_data_flags = []
+      is_blocked_flags = []
+      last_logins = []
       @csv.each do |r|
         first_names << r[:first_name] if r[:first_name]
         last_names << r[:last_name] if r[:last_name]
+        uids << r[:uid] if r[:uid]
+        titles << r[:title] if r[:title]
         emails << r[:email] if r[:email]
+        departments << r[:departments] if r[:departments]
+        drop_in_advising_flags << r[:drop_in_advising] if r[:drop_in_advising]
+        can_access_canvas_data_flags << r[:can_access_canvas_data]
+        is_blocked_flags << r[:is_blocked]
+        last_logins << r[:last_login] if r[:last_login]
       end
       logger.warn "The export CSV has #{@csv.count} rows, with #{first_names.length} first names, #{last_names.length} last names, and #{emails.length} emails"
       expect(first_names).not_to be_empty
       expect(last_names).not_to be_empty
+      expect(uids).not_to be_empty
+      expect(titles).not_to be_empty
       expect(emails).not_to be_empty
+      expect(departments).not_to be_empty
+      expect(drop_in_advising_flags).not_to be_empty
+      expect(can_access_canvas_data_flags).not_to be_empty
+      expect(is_blocked_flags).not_to be_empty
+      expect(last_logins).not_to be_empty
     end
   end
 end
