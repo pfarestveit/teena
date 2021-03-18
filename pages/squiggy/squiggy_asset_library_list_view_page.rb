af9cdb840cc5401f43f9ea0e4959f62a62d2929b
Paul Farestveit <pfarestveit@berkeley.edu>
2021-03-18 15:35:31 -0700

    UI interactions with Squiggy assets and comments

--- pages/squiggy/squiggy_asset_library_list_view_page.rb
+++ pages/squiggy/squiggy_asset_library_list_view_page.rb
@@ -6,10 +6,22 @@ class SquiggyAssetLibraryListViewPage
   include SquiggyAssetLibrarySearchForm
   include SquiggyAssetLibraryMetadataForm
 
+  ### UPLOADING FILES
+
   button(:upload_button, id: 'go-upload-asset-btn')
+
+  def click_upload_file_button
+    wait_for_update_and_click_js upload_button_element
+  end
+
+  def upload_file_asset(asset)
+    click_upload_file_button
+    enter_and_upload_file asset
+  end
+
+  ### ADDING LINKS
+
   button(:add_link_button, id: 'go-add-link-asset-btn')
-  button(:manage_assets_button, id: 'manage-assets-btn')
-  elements(:asset, :div, xpath: '//div[starts-with(@id, "asset-")]')
 
   def click_add_url_button
     wait_for_update_and_click_js add_link_button_element
@@ -17,13 +29,33 @@ class SquiggyAssetLibraryListViewPage
 
   def add_link_asset(asset)
     click_add_url_button
-    enter_url_metadata asset
+    enter_url asset
+    enter_asset_metadata asset
     click_save_link_button
+  end
+
+  # MANAGE ASSETS
+
+  button(:manage_assets_button, id: 'manage-assets-btn')
+
+  def click_manage_assets_link
+    wait_for_load_and_click manage_assets_button_element
+  end
+
+  # LIST VIEW ASSETS
+
+  elements(:asset, :div, xpath: '//div[starts-with(@id, "asset-")]')
+
+  def load_page(test)
+    navigate_to test.course.asset_library_url
+    wait_until(Utils.medium_wait) { title == SquiggyTool::ASSET_LIBRARY.name }
+    hide_canvas_footer_and_popup
+    switch_to_canvas_iframe
   end
 
   def wait_for_assets
     wait_until(Utils.short_wait) { asset_elements.any? }
-    sleep 1
+    sleep Utils.click_wait
   end
 
   def asset_xpath(asset)
@@ -32,6 +64,20 @@ class SquiggyAssetLibraryListViewPage
 
   def asset_el(asset)
     div_element(asset_xpath asset)
+  end
+
+  def canvas_submission_title(asset)
+    # For Canvas submissions, the file name or the URL are used as the asset title
+    asset.title = (asset.type == 'File') ? asset.file_name.sub(/\..*/, '') : asset.url
+  end
+
+  def load_list_view_asset(test, asset)
+    load_page test
+    wait_until(Utils.medium_wait) do
+      scroll_to_bottom
+      sleep 1
+      asset_el(asset).exists?
+    end
   end
 
   def visible_list_view_asset_data(asset)
@@ -48,10 +94,6 @@ class SquiggyAssetLibraryListViewPage
       like_count: (like_count_el.text.strip.to_i if like_count_el.exists?),
       comment_count: (comment_count_el.text.strip.to_i if comment_count_el.exists?)
     }
-  end
-
-  def click_manage_assets_link
-    wait_for_load_and_click manage_assets_button_element
   end
 
   def click_asset_link(asset)
