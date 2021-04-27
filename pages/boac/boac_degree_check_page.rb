6543382b3f4ba148a986c89f55811c92f66b38de
Paul Farestveit <pfarestveit@berkeley.edu>
2021-04-27 15:56:47 -0700

    Tests for degree check notes

--- pages/boac/boac_degree_check_page.rb
+++ pages/boac/boac_degree_check_page.rb
@@ -1,5 +1,51 @@
 class BOACDegreeCheckPage < BOACDegreeCheckTemplatePage
 
+  include PageObject
+  include Page
+  include Logging
+  include BOACPages
+
   div(:last_updated_msg, xpath: '//div[contains(text(), "Last updated by")]')
+
+  # NOTES
+
+  div(:no_notes_msg, id: 'degree-note-no-data')
+  button(:create_or_edit_note_button, id: 'create-degree-note-btn')
+  button(:print_note_toggle, id: 'degree-note-print-toggle')
+  div(:note_update_advisor, id: 'degree-note-updated-by')
+  div(:note_update_date, id: 'degree-note-updated-at')
+  text_area(:note_input, id: 'degree-note-input')
+  button(:save_note_button, id: 'save-degree-note-btn')
+  button(:cancel_note_button, id: 'cancel-degree-note-btn')
+  paragraph(:note_body, id: 'degree-note-body')
+
+  def click_create_or_edit_note
+    wait_for_update_and_click create_or_edit_note_button_element
+  end
+
+  def enter_note_body(string)
+    wait_for_element_and_type(note_input_element, string)
+  end
+
+  def click_save_note
+    wait_for_update_and_click save_note_button_element
+  end
+
+  def click_cancel_note
+    wait_for_update_and_click cancel_note_button_element
+    cancel_note_button_element.when_not_present 1
+  end
+
+  def create_or_edit_note(string)
+    logger.info "Entering degree note '#{string}'"
+    click_create_or_edit_note
+    enter_note_body string
+    click_save_note
+  end
+
+  def visible_note_body
+    note_body_element.when_visible Utils.short_wait
+    note_body
+  end
 
 end
