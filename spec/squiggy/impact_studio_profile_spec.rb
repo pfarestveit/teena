bf734a8e77e67264b9f1a929c3e5d0d1f3eccbfd
pfarestveit <pfarestveit@berkeley.edu>
2022-12-15 14:38:41 -0800

    Squash IS profile scripts

--- spec/squiggy/impact_studio_profile_spec.rb
+++ spec/squiggy/impact_studio_profile_spec.rb
@@ -2,11 +2,13 @@ require_relative '../../util/spec_helper'
 
 describe 'Impact Studio' do
 
-  test = SquiggyTestConfig.new 'profile_summary'
-
+  test = SquiggyTestConfig.new 'studio_profile'
+  teacher = test.teachers[0]
   student_1 = test.students[0]
   student_2 = test.students[1]
   student_3 = test.students[2]
+  students = [student_1, student_2, student_3]
+  test.course.roster -= students
 
   before(:all) do
     @driver = Utils.launch_browser
@@ -18,14 +20,125 @@ describe 'Impact Studio' do
 
     @canvas.log_in(@cal_net, test.admin.username, Utils.super_admin_password)
     @canvas.create_squiggy_course test
-    @engagement_index.wait_for_new_user_sync(test, test.course.roster)
   end
 
   after(:all) { Utils.quit_browser @driver }
 
+  context 'when there are fewer than two members of the course site' do
+
+    context 'and the course site member views its profile' do
+
+      before(:all) do
+        @canvas.masquerade_as(teacher, test.course)
+        @impact_studio.load_own_profile(test, teacher)
+      end
+
+      it 'offers a user search field' do
+        @impact_studio.user_select_element.when_present Utils.short_wait
+        expect(@impact_studio.user_select_options.map &:strip).to eql([teacher.full_name])
+      end
+      it 'offers user profile pagination' do
+        expect(@impact_studio.browse_previous_element.text).to include(teacher.full_name)
+        expect(@impact_studio.browse_next_element.text).to include(teacher.full_name)
+      end
+    end
+  end
+
+  context 'when there are exactly two members of the course site' do
+
+    before(:all) do
+      @canvas.stop_masquerading
+      @canvas.add_users_by_section(test.course, [student_1])
+    end
+
+    context 'and a course site member views its profile' do
+
+      before(:all) do
+        @canvas.masquerade_as(student_1, test.course)
+        @impact_studio.load_own_profile(test, student_1)
+      end
+
+      it 'offers a user search field' do
+        @impact_studio.user_select_element.when_present Utils.short_wait
+        expect(@impact_studio.user_select_options.map &:strip).to eql([teacher.full_name, student_1.full_name])
+      end
+      it 'offers user profile pagination' do
+        expect(@impact_studio.browse_previous_element.text).to include(teacher.full_name)
+        expect(@impact_studio.browse_next_element.text).to include(teacher.full_name)
+      end
+    end
+  end
+
+  context 'when there are more than two members of the course site' do
+
+    before(:all) do
+      @canvas.stop_masquerading
+      @canvas.add_users_by_section(test.course, [student_2])
+    end
+
+    context 'and a course site member views its profile' do
+
+      before(:all) do
+        @canvas.masquerade_as(student_2, test.course)
+        @impact_studio.load_own_profile(test, student_2)
+      end
+
+      it 'offers a user search field' do
+        @impact_studio.user_select_element.when_present Utils.short_wait
+        expect(@impact_studio.user_select_options.map &:strip).to eql([teacher.full_name, student_1.full_name, student_2.full_name])
+      end
+      it 'offers user profile pagination' do
+        expect(@impact_studio.browse_previous_element.text).to include(student_1.full_name)
+        expect(@impact_studio.browse_next_element.text).to include(teacher.full_name)
+      end
+    end
+  end
+
+  describe 'search' do
+
+    before(:all) do
+      @canvas.masquerade_as(teacher, test.course)
+      @impact_studio.load_own_profile(test, teacher)
+    end
+
+    [student_1, student_2].each do |student|
+      it "allows the user to view UID #{student.uid}'s profile" do
+        @impact_studio.select_user student
+        @impact_studio.wait_for_profile student
+      end
+    end
+  end
+
+  describe 'pagination' do
+
+    before(:all) do
+      @users = [teacher, student_1, student_2]
+      @canvas.masquerade_as student_1
+      @impact_studio.load_own_profile(test, student_1)
+    end
+
+    it 'allows the user to page next through each course site member' do
+      @users.sort_by! { |u| u.full_name }
+      index_of_current_user = @users.index student_1
+      @users.rotate!(index_of_current_user + 1)
+      @users.each { |user| @impact_studio.browse_next_user user }
+    end
+
+    it 'allows the user to page previous through each course site member' do
+      @users.reverse!
+      index_of_current_user = @users.index(student_1)
+      @users.rotate!(index_of_current_user + 1)
+      @users.each { |user| @impact_studio.browse_previous_user user }
+    end
+  end
+
   describe 'profile summary' do
 
     before(:all) do
+      @canvas.stop_masquerading
+      @canvas.add_users_by_section(test.course, [student_3])
+      @engagement_index.wait_for_new_user_sync(test, [student_3])
+
       @asset = SquiggyAsset.new title: "Asset #{test.id}",
                                 url: 'www.google.com',
                                 description: 'BitterTogether'
