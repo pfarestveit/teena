95735994aafb0de43f73ed846e3ec112019fe9f5
Paul Farestveit <pfarestveit@berkeley.edu>
2024-08-13 11:15:20 -0700

    Site publishing UI has changed in Canvas

--- pages/canvas/canvas_page.rb
+++ pages/canvas/canvas_page.rb
@@ -404,24 +404,28 @@ module Page
     button(:publish_button, class: 'btn-publish')
     button(:save_and_publish_button, class: 'save_and_publish')
     button(:published_button, class: 'btn-published')
-    form(:published_status, id: 'course_status_form')
+    button(:publish_status, xpath: '//button[@data-position-target="course_publish_menu"]')
+    button(:published_status, xpath: '//button[contains(., "Published")]')
+    button(:unpublished_status, xpath: '//button[contains(., "Unpublished")]')
+    button(:publish_site_button, xpath: '//button[contains(., "Publish")]')
     radio_button(:activity_stream_radio, xpath: '//span[contains(.,"Course Activity Stream")]/ancestor::label')
     button(:choose_and_publish_button, xpath: '//span[contains(.,"Choose and Publish")]/ancestor::button')
 
     def publish_course_site(course_site)
       logger.info 'Publishing the course'
       load_course_site course_site
-      published_status_element.when_visible Utils.short_wait
-      if published_button?
-        logger.debug 'The site is already published'
-      else
+      publish_status_element.when_visible Utils.short_wait
+      if unpublished_status?
         logger.debug 'The site is unpublished, publishing'
-        js_click publish_button_element
+        js_click unpublished_status_element
+        js_click publish_site_button_element
         unless course_site.create_site_workflow || course_site.is_copy
           wait_for_update_and_click activity_stream_radio_element
           wait_for_update_and_click choose_and_publish_button_element
         end
-        published_button_element.when_present Utils.medium_wait
+        published_status_element.when_present Utils.medium_wait
+      else
+        logger.debug 'The site is already published'
       end
     end
 
